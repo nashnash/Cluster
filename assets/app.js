504bc44e04af37be50bcbb08c0370e17f0d06e07
Théo SIKLI <theo@theo-sikli.fr>
2020-12-04 21:39:03 +0100

    Install bootstrap css/js

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,16 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
