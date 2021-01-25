ae5f4eaadce3db27c792dc38da629613d62b5f35
Nashad <nashad@macbook-pro-de-nashad.home>
2021-01-25 02:13:08 +0100

    add controller for index page + template twig for index & image

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ Encore
         from: './assets/images',
 
         // optional target path, relative to the output dir
-        //to: 'images/[path][name].[ext]',
+        to: 'images/[path]/[name].[ext]',
 
         // if versioning is enabled, add the file hash too
         //to: 'images/[path][name].[hash:8].[ext]',
