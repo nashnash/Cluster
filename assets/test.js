ab4c85cc7973970dbff027f7dfcafbf0761d4064
Théo SIKLI <11704898+Saikurin@users.noreply.github.com>
2021-01-29 21:41:25 +0100

    Delete test.js

--- assets/test.js
+++
@@ -1,7 +0,0 @@
-import getLocation from'./utils/geoloc';
-
-console.log(navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position);
-}))
-
-console.log(getLocation.getLocation());
