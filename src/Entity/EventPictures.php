9c27740bd1b15d2a6b8ba26d28fe9799d0f9613e
Jules <julesgabiam31@gmail.com>
2021-02-09 18:03:00 +0100

    correction class EventPictures

--- src/Entity/EventPictures.php
+++ src/Entity/EventPictures.php
@@ -11,7 +11,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity(repositoryClass=EventPicturesRepository::class)
  */
-class EventsPictures
+class EventPictures
 {
 
     public function __construct()
